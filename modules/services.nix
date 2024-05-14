a7d4969bc3db55c3e44ae28485ece613e8196914
Enrico PANETTA <enpanetta-ext@airfrance.fr>
2024-05-14 16:30:10 +0200

    Update ssh conf

--- modules/services.nix
+++ modules/services.nix
@@ -2,6 +2,6 @@
 {
   # Enable necessary services
   services.openssh.enable = true;
-  services.openssh.settings.permitRootLogin = "yes";
-  services.openssh.settings.passwordAuthentication = true;
+  services.openssh.settings.PermitRootLogin = "yes";
+  services.openssh.settings.PasswordAuthentication = true;
 }
