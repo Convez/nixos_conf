6b56ba66f9cc37bc6a706f4b67751f205ecf6a5e
Enrico Panetta <convezione@proton.me>
2024-06-04 12:20:46 +0000

    Homemanager gh cli

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,12 +3,12 @@
 {
   programs.gh = {
     enable = true;
-    gitCredentialsHelper.enable = true;
+    gitCredentialHelper.enable = true;
   };
   programs.git = {
     enable = true;
     lfs.enable = true;
     userName = "Convez";
     userEmail = "convezione@proton.me";
-  }
+  };
 }
--- modules/common.nix
+++ modules/common.nix
@@ -4,7 +4,6 @@
   environment.systemPackages = with pkgs; [
     wget
     curl
-    home-manager
   ];
 
   # Enable Flakes
