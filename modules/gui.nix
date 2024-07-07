336e5b135b6a63d8d0f39b80468a4b0d4f892175
Convez <convezione@proton.me>
2024-07-07 20:21:28 +0200

    Switch to kde

--- modules/gui.nix
+++ modules/gui.nix
@@ -4,7 +4,11 @@
   services.xserver.enable = true;
   
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmax11";
 
 }
