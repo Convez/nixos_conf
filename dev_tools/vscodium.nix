6279bc0bf392815d70c64162056985455cb324c9
Convez <convezione@proton.me>
2025-03-25 22:11:14 +0100

    Modernize vscodium configs

--- dev_tools/vscodium.nix
+++ dev_tools/vscodium.nix
@@ -6,14 +6,16 @@ in
   programs.vscode = {
     enable = cfg.ides.code;
     mutableExtensionsDir=true;
-    extensions = languages.codeExtensions;
-    userSettings = {
-      "editor.fontFamily"= "MesloLGS NF";
-      "terminal.integrated.persistentSessionReviveProcess"= "never";
-      "terminal.integrated.defaultProfile.linux"= "zsh";
-      "terminal.integrated.fontFamily"="MesloLGS NF";
-      "runtimeExecutable"= "/home/convez/.nix-profile/bin/node";
-    } //
-    languages.codeSettings;
+    profiles.default = {
+      extensions = languages.codeExtensions;
+      userSettings = {
+        "editor.fontFamily"= "MesloLGS NF";
+        "terminal.integrated.persistentSessionReviveProcess"= "never";
+        "terminal.integrated.defaultProfile.linux"= "zsh";
+        "terminal.integrated.fontFamily"="MesloLGS NF";
+        "runtimeExecutable"= "/home/convez/.nix-profile/bin/node";
+      } //
+      languages.codeSettings;
+    };
   };
 }
