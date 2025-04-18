7723c05769aa6a354aeefafd81a85787a183e7a2
Enrico Panetta <convezione@proton.me>
2025-04-18 11:59:24 +0200

    Add vim colorscheme

---
+++ home/modules/dev_tools/neovim/config/after/plugin/colors.lua
@@ -0,0 +1,12 @@
+function SetupColors(color)
+  color = color or "ayu-dark"
+  vim.cmd.colorscheme(color)	
+
+  -- Set transparent background
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+-- TODO: Add support for configuring colorschemes
+-- By project. Since we're already using direnv, maybe we can
+-- use a set the colorscheme in the .envrc file as an env variable
+SetupColors()
--- home/modules/dev_tools/neovim/config/lua/plugin/init.lua
+++ home/modules/dev_tools/neovim/config/lua/plugin/init.lua
@@ -9,6 +9,12 @@ local setup = function (use)
   -- use 'neovim/nvim-lspconfig'
   use 'mfussenegger/nvim-jdtls'
   use 'ThePrimeagen/vim-be-good'
+  
+  -- Colorschemes
+  use 'Shatur/neovim-ayu'
+  use 'sts10/vim-pink-moon'
+  use 'rose-pine/neovim'
+
 end
 
 packer.startup(setup)
