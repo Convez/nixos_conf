4beb384f5b2a760c1680a64aec86a95dd8fb73ca
Convez <convezione@proton.me>
2025-04-19 21:12:10 +0200

    Add relative line numbers

--- home/modules/dev_tools/neovim/config/lua/keymap/init.lua
+++ home/modules/dev_tools/neovim/config/lua/keymap/init.lua
@@ -2,6 +2,8 @@ print("Staring keymap config")
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<leader>e', ":Ex<CR>")
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
