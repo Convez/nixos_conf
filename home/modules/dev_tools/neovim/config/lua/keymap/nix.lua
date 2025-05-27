3647378258dbe96acf77a01138799edea27b4a3e
Convez <convezione@proton.me>
2025-05-27 22:59:21 +0200

    Wallpaper

--- home/modules/dev_tools/neovim/config/lua/keymap/nix.lua
+++ home/modules/dev_tools/neovim/config/lua/keymap/nix.lua
@@ -17,3 +17,16 @@ local row = curs_pos[1];
 local col = curs_pos[2];
 vim.api.nvim_buf_set_lines(0, row - 1,row-1,false, input)
 end)
+
+-- Write .envrc for new project
+vim.keymap.set('n', '<leader>envrc', function ()
+local input = {
+	"use flake",
+	"export IN_NIX_SHELL=\"convez#<PROJECT_NAME>\"",
+	"export PROJECT_LANGS=$(github-linguist . -j | jq --raw-output 'to_entries|sort_by(.value.percentage)|reverse|map(.key)|@csv|gsub(\"\\\"\";\"\")')"
+}
+local curs_pos = vim.api.nvim_win_get_cursor(0);
+local row = curs_pos[1];
+local col = curs_pos[2];
+vim.api.nvim_buf_set_lines(0, row - 1,row-1,false, input)
+end)
--- os/audio/default.nix
+++ os/audio/default.nix
@@ -7,19 +7,5 @@
 			alsa.support32Bit = true;
 			pulse.enable = true;
 		};
-		pulseaudio = {
-			enable = lib.mkForce false;
-			support32Bit = true;
-			package = pkgs.pulseaudioFull;
-			configFile = pkgs.writeText "default.pa" ''
-			load-module module-bluetooth-policy
-			load-module module-bluetooth-discover
-			## module fails to load with 
-			##   module-bluez5-device.c: Failed to get device path from module arguments
-			##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
-			# load-module module-bluez5-device
-			# load-module module-bluez5-discover
-			'';
-		};
 	};
  }
